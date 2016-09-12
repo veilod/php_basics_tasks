506bf937b40d9b32e49c97bba53307f60dd50bf0
Igor R <bocharova135@gmail.com>
2016-09-12 10:36:40 +0300

    add 19 , 22

---
+++ 19.php
@@ -0,0 +1,14 @@
+<?php
+include "17.php";
+/**
+ * Created by PhpStorm.
+ * User: IgorPc
+ * Date: 12.09.2016
+ * Time: 10:33
+ */
+
+echo "<br>";
+var_dump($a);
+echo "<br>";
+var_dump($b);
+
---
+++ 22.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: IgorPc
+ * Date: 12.09.2016
+ * Time: 10:31
+ */
+
+$num = -20;
+var_dump($num);
+echo "<br>";
+settype($num , 'boolean');
+var_dump($num);
